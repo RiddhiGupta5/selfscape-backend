8f820bfa542ec62b403c503bb738441338864b60
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-09-01 00:28:29 +0530

    Added database and all the user routes

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const app = express();
 
 const userRoutes = require("./api/routes/userRoutes");
 
+mongoose.connect(
+  "mongodb+srv://RiddhiGupta5:" +
+    process.env.MONGO_ATLAS_PASSWORD +
+    "@selfscapecluster.oeoxz.mongodb.net/selfscapeDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+mongoose.Promise = global.Promise;
+
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -39,9 +50,8 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
-    error: {
-      message: error.message,
-    },
+    message: "Something went Wrong",
+    error: error.message,
   });
 });
 
